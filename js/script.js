9e034fd93cecf13a2abb5960a6529033cf3d3a34
esraaali138 <124624095+esraaali138@users.noreply.github.com>
2023-03-09 23:16:29 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// document.getElementById("btn").onclick = function () {
+//   document.getElementById("div1").innerHTML = "confir"
+// }
+
+var x=30
+x = null
+console.log(x)
+console.log(typeof x)
+  
